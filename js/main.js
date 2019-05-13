c79bef9ab370b408165e8212d0330688908305ea
daafisi <daafisidaa@gmail.com>
2019-05-13 19:27:25 +0200

    app online

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Make sure sw are supported
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('../sw_cached_site.js')
+      .then(reg => console.log('Service Worker: Registered (Pages)'))
+      .catch(err => console.log(`Service Worker: Error: ${err}`));
+  });
+}
+
+
+var app = new Vue({ 
+  el: '#app',
+  data: {
+      playerLifeTotal: 20,
+      name: 'John',
+      oppLifeTotal: 20
+  },
+  methods: {
+      plusOne : function() {
+          this.playerLifeTotal +=1
+          //console.log(this.playerLifeTotal)
+      },
+      minusOne : function() {
+          this.playerLifeTotal -=1
+      },
+      oppplusOne : function() {
+          this.oppLifeTotal +=1
+          //console.log(this.playerLifeTotal)
+      },
+      oppminusOne : function() {
+          this.oppLifeTotal -=1
+      },
+      resetLife : function() {
+          this.oppLifeTotal = 20
+          this.playerLifeTotal = 20
+      }
+  }
+});
